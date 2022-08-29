e288ccd119f68eda5fc98d124bce4cf69d52a756
0321minji <79096808+0321minji@users.noreply.github.com>
2022-08-29 13:30:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 64 ms, Memory: 2776 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+bool compare(pair<int, int>a, pair<int, int>b) {
+	if (a.second == b.second) {
+		return a.first < b.first;
+	}
+	else {
+		return a.second < b.second;
+	}
+}
+
+
+int main() {
+	int N, x, y;
+	vector<pair<int, int>> v;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &x, &y);
+		v.push_back(make_pair(x, y)); //pair를 만들어서 vector에 삽입
+	}
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < N; i++) {
+		printf("%d %d\n", v[i].first, v[i].second);
+	}
+}
