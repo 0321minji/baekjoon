b996a6baaf613d72c019e9920d2bbe3849ab2ba6
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-17 00:21:10 +0900

    [Bronze I] Title: 전화 요금, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/3226. 전화 요금/전화 요금.cc
+++ 백준/Bronze/3226. 전화 요금/전화 요금.cc
@@ -2,42 +2,33 @@
 #include<string>
 using namespace std;
 
-int cal(int h, int m,int time,int charge) {
-	int end_h = h;
-	int end_m = (m + time) % 60;
-
-	if (m + time >= 60) {
-		end_h += 1;
-		if (end_h == 24) {
-			end_h = 0;
-		}
-	}
-	if (h == 6 && end_h == 7) {
-		charge += (time - end_m) * 5 + end_m * 10;
-	}
-	else if (h == 18 && end_h == 19) {
-		charge += (time - end_m) * 10 + end_m * 5;
-	}
-	else {
-		if (6 < h && h < 19) {
-			charge += 10 * time;
+int calculateCharge(int h, int m, int time) {
+	int ans = 0;
+	while (time--) {
+		if (h >= 7 && h < 19) {
+			ans += 10;
 		}
 		else {
-			charge += 5 * time;
+			ans += 5;
 		}
+		h += (++m / 60) % 24;
+		m %= 60;
 	}
-	return charge;
+	return ans;
 }
 
 int main() {
-	int n, h, m, time;
+	int n, m, time;
 	int charge = 0;
-	string hh;
+	string h;
+
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		getline(cin, hh, ':');
+
+	while(n--){
+		getline(cin, h, ':');
 		cin >> m >> time;
-		charge = cal(stoi(hh), m, time,charge);
+		charge+= calculateCharge(stoi(h),m,time);
 	}
+
 	cout << charge;
 }
