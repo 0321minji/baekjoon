4204195fd0ddd4db479dee04fe5368384f82e3ce
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-25 11:10:26 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -8,6 +8,7 @@ score.sort(key=lambda x : (-x[1], -x[2], -x[3]))
 for i in range(n):
     if score[i][0] == k:
         index = i
+        break
 for i in range(n):
     if score[index][1:] == score[i][1:]:
         print(i + 1)
