00eaf582e358fed92031102038b770bbba62cd39
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-25 13:06:49 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,19 +1,16 @@
 def solution(n, lost, reserve):
     answer = 0
-    clothes=[1]*(n+2)
-    #2만큼 여유를 둬서 예외 상황 처리하기
-    for i in lost:
-        clothes[i]-=1
-    for i in reserve:
-        clothes[i]+=1
-    for i in range(n+1):
-        if clothes[i]==0 and clothes[i-1]>1:
-            clothes[i]+=1; clothes[i-1]-=1
+    #가지고 온 사람이 도난당한 경우 제거
+    reserved=set(reserve)-set(lost)
+    losted=set(lost)-set(reserve)
+    
+    for i in sorted(reserved):
+        if i-1 in losted:
+            losted-={i-1}
             continue
-        if clothes[i]==0 and clothes[i+1]>1:
-            clothes[i]+=1; clothes[i+1]-=1
-    print(clothes)
-    for i in range(1,n+1):
-        if clothes[i]>=1:
-            answer+=1
+        if i+1 in losted:
+            losted-={i+1}
+
+    answer=n-len(losted)
+
     return answer
