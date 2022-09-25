787cea0efa1b0f2fd4391595dc48b48866657a55
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-25 11:08:59 +0900

    [Silver V] Title: 올림픽, Time: 120 ms, Memory: 114408 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+score = []
+for i in range(n):
+    score.append(list(map(int, input().split())))
+score.sort(key=lambda x : (-x[1], -x[2], -x[3]))
+for i in range(n):
+    if score[i][0] == k:
+        index = i
+for i in range(n):
+    if score[index][1:] == score[i][1:]:
+        print(i + 1)
+        break  
