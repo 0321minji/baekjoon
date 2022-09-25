59c63aa1e07c2287ba64b52e6d52761aa4ccbfae
밍 <79096808+0321minji@users.noreply.github.com>
2022-09-25 10:58:01 +0900

    [Silver II] Title: 키로거, Time: 1376 ms, Memory: 352624 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -1,30 +1,26 @@
 import sys
 input=sys.stdin.readline
 from collections import deque
-
-def solve(t):
+def solve(s):
     left=[]
     right=[]
-    t=deque(t)
-    index=0
-    while t:
-        temp=t.popleft()
-        if temp=='<':
+    s=deque(s)
+    while s:
+        temp=s.popleft()
+        if temp=='-':
+            if left:
+                left.pop()
+        elif temp=='<':
             if left:
                 right.append(left.pop())
         elif temp=='>':
             if right:
                 left.append(right.pop())
-        elif temp=='-':
-            if left:
-                left.pop()
         else:
             left.append(temp)
     return left+right[::-1]
-    
-    
-n=int(input())
-for i in range(n):
-    t=list(input().rstrip())
-    print(*solve(t),sep='')
-    
+
+t=int(input())
+for i in range(t):
+    s=list(input().strip())
+    print(*solve(s),sep='')
