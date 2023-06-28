20a3e86d2ce8f12953caa281165c15384cd005fc
밍 <79096808+0321minji@users.noreply.github.com>
2023-06-28 17:23:46 +0900

    [Silver V] Title: 다리 놓기, Time: 172 ms, Memory: 115392 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,19 +1,15 @@
 t=int(input())
 
+def fact(n):
+    if n==1:
+        return n
+    else:
+        return(n*fact(n-1))
+        
 for i in range(t):
-    n,m=map(int,input().split())
-    dp=[[0]*(m+1) for _ in range(n+1)]
-    for j in range(1,n+1):
-        for k in range(1,m+1):
-            if j==1:
-                dp[j][k]=k
-                continue
-            if j==k:
-                dp[j][k]=1
-            else:
-                if k>j:
-                    dp[j][k]=dp[j][k-1]+dp[j-1][k-1]
-
-    print(dp[n][m])
- 
-   
+    a,b=map(int,input().split())
+    if a==b:
+        print(1)
+        continue
+    temp=fact(b)//fact(a)//fact(b-a)
+    print(temp)
