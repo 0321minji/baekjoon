d585e1f907f251b464877114fbd232c711de3d2a
밍 <79096808+0321minji@users.noreply.github.com>
2023-10-24 16:33:56 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,17 +1,17 @@
 import sys
 input=sys.stdin.readline
 
-def solve(n,a,b):
-    a.sort()
-    b.sort(reverse=True)
-    result=0
-    for i in range(n):
-        result+=a[i]*b[i]
-    print(result)
 
-
-    
 n=int(input())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
-solve(n,a,b)
+##b 정렬 불가 ..?? ㅁㄹㄹ몸ㄹ라재배열할거임
+b=sorted(b,reverse=True)
+a.sort()
+
+s=0
+for i in range(n):
+    s+=a[i]*b[i]
+
+print(s)
+
