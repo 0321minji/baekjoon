1f00063632af9976f2e754b1d068380584b0e117
밍 <79096808+0321minji@users.noreply.github.com>
2023-10-31 16:46:13 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
@@ -1,8 +1,15 @@
-R, B = map(int, input().split())
-LpW = (R + 4) // 2
-LW = R + B
+import sys
+input=sys.stdin.readline
 
-for i in range(1, LpW):
-    if (LpW - i) * i == LW:
-        print(max(i, LpW-i), min(i, LpW-i))
+r,b=map(int,input().split())
+
+t=r+b
+
+for i in range(3,t//2):
+    if t%i!=0:
+        continue
+    temp=t//i
+    if (i-2)*(temp-2)==b:
+        print(temp,i)
         break
+    
