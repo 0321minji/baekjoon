908a87c1269f7ac4004b113bbccfa2d4c713978b
밍 <79096808+0321minji@users.noreply.github.com>
2023-10-31 15:20:01 +0900

    [Gold II] Title: 후위 표기식, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -1,34 +1,32 @@
 import sys
 input=sys.stdin.readline
 
-temp=list(map(str,input().strip()))
-stack=[]
+inp=list(input().rstrip())
+st=[]
 result=''
 
-for i in temp:
-    if i.isalpha():
-        result+=i
-    elif i=='(':
-        stack.append(i)
-        
-    #연산자와 동일 우선순위일 경우 이전 연산자 우선
-    elif i=='*' or i=='/':
-        while stack and (stack[-1]=='*' or stack[-1]=='/'):
-            result+=stack.pop()
-        stack.append(i)
+for temp in inp:
+    if temp.isalpha():
+        result+=temp
 
-    #괄호 안에 들어있는 경우 + or -의 우선순위가 높음
-    #그 외의 경우 이전 연산자 우선순위가 높기에 pop한 후에 append
-    elif i== '+' or i=='-':
-        while stack and stack[-1]!='(':
-            result+=stack.pop()
-        stack.append(i)
-    elif i==')':
-        while stack and stack[-1]!='(':
-            result+=stack.pop()
-        stack.pop()
+    elif temp=='(':
+        st.append(temp)
 
-while stack:
-    result+=stack.pop()
+    elif temp =='*' or temp=='/':
+        while st and (st[-1]=='*' or st[-1]=='/'):
+            result+=st.pop()
+        st.append(temp)
 
+    elif temp=='+' or temp== '-':
+        while st and st[-1]!='(':
+            result+=st.pop()
+        st.append(temp)
+
+    elif temp==')':
+        while st and st[-1]!='(':
+            result+=st.pop()
+        st.pop()
+
+while st:
+    result+=st.pop()
 print(result)
