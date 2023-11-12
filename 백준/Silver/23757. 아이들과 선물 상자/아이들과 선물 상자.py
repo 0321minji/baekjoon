68facceaf503de61007c00cd81e3aa86b3ecc497
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-12 22:21:39 +0900

    [Silver II] Title: 아이들과 선물 상자, Time: 236 ms, Memory: 138144 KB -BaekjoonHub

--- 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.py
+++ 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.py
@@ -1,20 +1,20 @@
-import heapq as hq
-# python에서는 기본적으로 최소 힙이므로 문제 해결을 위해 최대힙으로 변경
-n, m = map(int, input().split())
-present = []
-for x in list(map(int, input().split())):
-    hq.heappush(present, -x) # 최대 힙 구현을 위하여 인자 -로 삽입
-wish = list(map(int, input().split()))
+import sys, heapq
+input=sys.stdin.readline
 
-ispre = False # 선물이 남는지 안남는지 확인 False 일 시 선물이 아이들 모두 분배
-for i in wish:
-    x = -hq.heappop(present)
-    if x - i < 0:
-        ispre = True # True 면 선물이 부족하다는 뜻
+n,m=map(int,input().split())
+c=list(map(int,input().split()))
+w=list(map(int,input().split()))
+
+pq=[]
+for i in range(n):
+    heapq.heappush(pq,-c[i])
+
+result=1
+for i in range(m):
+    temp=heapq.heappop(pq)
+    if w[i]>-temp:
+        result=0
         break
-    hq.heappush(present, -(x-i))
+    heapq.heappush(pq,-(-temp-w[i]))
 
-if ispre:
-    print(0)
-else:
-    print(1)
+print(result)
