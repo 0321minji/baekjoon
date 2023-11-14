d88c7992b5062ae9727bde52be7c203554b45e0e
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-14 16:22:00 +0900

    [Gold IV] Title: 루머, Time: 5828 ms, Memory: 202312 KB -BaekjoonHub

--- 백준/Gold/19538. 루머/루머.py
+++ 백준/Gold/19538. 루머/루머.py
@@ -1,43 +1,43 @@
+import sys
 from collections import deque
+input=sys.stdin.readline
+
+def solve():
+    que=deque([])
+    while rumor:
+        index=rumor.popleft()
+        temp=people[index]
+        for i in temp:
+            if time[i]!=-1:
+                continue
+            count=0
+            for k in range(len(people[i])):
+                if time[people[i][k]]!=-1:
+                    count+=1
+            if len(people[i])>count*2:
+                continue
+            que.append(i)
+        #해당 시간에 대한 탐색이 끝
+        if not rumor:
+            while que:
+                tmp=que.popleft()
+                time[tmp]=time[index]+1
+                rumor.append(tmp)
+    print(*time[1:])
+
 n=int(input())
 people=[]
-for i in range(n):
-    t=list(map(int,input().split()))
-    t.pop()
-    people.append(t)
+for _ in range(n):
+    temp=list(map(int,input().split()))
+    people.append(temp[:-1])
+
 people.insert(0,[])
 m=int(input())
-start=list(map(int,input().split()))
+rumor=list(map(int,input().split()))
 
 time=[-1]*(n+1)
-for i in start:
+for i in rumor:
     time[i]=0
-##최초유포자 0으로 초기화
-#1번부터 시작 유의**
 
-start=deque(start)
-move=[]
-que=deque([])
-while(start):
-    index=start.popleft()
-    temp=people[index]
-    move=[]
-    for i in temp:
-        count=0
-        if time[i]!=-1:
-            continue ##이전에 바뀜
-        leng=len(people[i])
-        for j in range(leng):
-            if time[people[i][j]]!=-1:
-                count+=1
-        if leng>count*2:
-            continue
-        que.append(i)
-    if not start:
-        while(que):
-            tmp=que.popleft()
-            time[tmp]=time[index]+1
-            start.append(tmp)
-
-time.remove(time[0])
-print(*time)
+rumor=deque(rumor)
+solve()
