7b6895a0346b104a9dbe91cc80d31636feb37147
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-14 15:19:17 +0900

    [Silver I] Title: 안전 영역, Time: 272 ms, Memory: 115468 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -2,35 +2,35 @@ import sys
 input=sys.stdin.readline
 
 move=[[0,1],[0,-1],[1,0],[-1,0]]
-def solve(a,b,v,visit):
+
+def solve(a,b,k,visit):
     st=[[a,b]]
     while st:
-        a,b=st.pop()
-        visit[a][b]=1
-        for dx,dy in move:
-            nx=a+dx; ny=b+dy
-            if 0<=nx<n and 0<=ny<n:
-                if MAP[nx][ny]>v and not visit[nx][ny]:
-                    st.append([nx,ny])
-                    visit[nx][ny]=1
+        x,y=st.pop()
+        for a,b in move:
+            nx=x+a; ny=y+b;
+            if 0<=nx<n and 0<=ny<n and h[nx][ny]>k and not visit[nx][ny]:
+                st.append([nx,ny])
+                visit[nx][ny]=1
+                
+
     
 n=int(input())
-MAP=[]
+h=[list(map(int,input().split())) for _ in range(n)]
+
 result=0
+height=max(map(max,h))
 
-for _ in range(n):
-    MAP.append(list(map(int,input().split())))
-
-MAX=max(map(max,MAP))
-for i in range(MAX):
-    visit=[[0]*n for _ in range(n)]
+for k in range(height):
     temp=0
-
-    for j in range(n):
-        for k in range(n):
-            if MAP[j][k]>i and not visit[j][k]:
-                solve(j,k,i,visit)
+    visit=[[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if h[i][j]>k and not visit[i][j]:
+                solve(i,j,k,visit)
                 temp+=1
+                visit[i][j]=1
 
     result=max(temp,result)
+
 print(result)
