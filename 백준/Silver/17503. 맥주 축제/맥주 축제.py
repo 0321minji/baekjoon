5e2c3ec798ec350a964673158fd35c1473dc6b52
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-21 22:46:18 +0900

    [Silver I] Title: 맥주 축제, Time: 788 ms, Memory: 152028 KB -BaekjoonHub

--- 백준/Silver/17503. 맥주 축제/맥주 축제.py
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.py
@@ -2,24 +2,21 @@ import sys, heapq
 input=sys.stdin.readline
 
 n,m,k=map(int,input().split())
-bear=[]
-for _ in range(k):
-    bear.append(list(map(int,input().split())))
-bear=sorted(bear,key=lambda x:(x[1],x[0]))
-#도수 레벨 순으로 정렬
+beer=[list(map(int,input().split())) for _ in range(k)]
+beer=sorted(beer,key=lambda x:x[1])
 
-p=0
+temp=0
 pq=[]
-answer=-1
-for i in bear:
-    p+=i[0]
+result=-1
+
+for i in beer:
+    temp+=i[0]
     heapq.heappush(pq,i[0])
 
     if len(pq)==n:
-        if p>=m:
-            answer=i[1]
+        if temp>=m:
+            result=i[1]
             break
         else:
-            p-=heapq.heappop(pq)
-
-print(answer)
+            temp-=heapq.heappop(pq)
+print(result)
