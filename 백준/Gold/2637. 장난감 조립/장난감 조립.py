7a35c56adcbd532c969e9a083a9e9e712ac01e49
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-21 22:39:18 +0900

    [Gold II] Title: 장난감 조립, Time: 144 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -1,34 +1,31 @@
-import sys 
-input=sys.stdin.readline
+import sys
 from collections import deque
+input=sys.stdin.readline
 
 n=int(input())
 m=int(input())
 part=[[] for _ in range(n+1)]
-need=[0]*(n+1)
 result=[0]*(n+1)
 result[n]=1
-
+need=[0]*(n+1)
 
 for _ in range(m):
     x,y,k=map(int,input().split())
-    part[x].append((y,k))
+    part[x].append([y,k])
     need[y]+=1
 
 dq=deque([n])
 
-origin =[i for i in range(1,n+1) if not part[i]]
+#기본 부품 번호
+origin=[i for i in range(1,n+1) if not part[i]]
+
 while dq:
     temp=dq.popleft()
     for nxt, cnt in part[temp]:
         need[nxt]-=1
         result[nxt]+=cnt*result[temp]
-        
+
         if not need[nxt]:
             dq.append(nxt)
-
-#for i in range(1,n+1):
-#   print(i,result[i])
-
 for i in origin:
     print(i,result[i])
