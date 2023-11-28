9634b8cac30eb0af5d63422f3d126e2de6e8aebd
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-28 12:23:35 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,21 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    wear={}
+
+    for _ in range(n):
+        temp=list(input().split())
+        t=temp[1]
+        if t in wear:
+            wear[t].append(temp[0])
+        else:
+            wear[t]=[temp[0]]
+    result=1
+    for i in wear:
+        result*=(len(wear[i])+1)
+        #종류수+1 곱해줌
+    print(result-1)
+        
