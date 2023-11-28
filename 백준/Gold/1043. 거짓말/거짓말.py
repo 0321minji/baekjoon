f3d1f247121e59672314c8cb580f3a383773c65e
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-28 22:57:24 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,6 +1,6 @@
 import sys
 input=sys.stdin.readline
-
+##union-find
 
 def find(parent,x):
     if x!=parent[x]:
@@ -8,28 +8,29 @@ def find(parent,x):
     return parent[x]
 
 def union(parent,a,b,mem):
-    a=find(parent,a)
-    b=find(parent,b)
+    ap=find(parent,a)
+    bp=find(parent,b)
 
-    if a in mem and b in mem:
+    #진실을 아는 사람이면 그냥 return
+    if ap in mem and bp in mem:
         return
-
-    if a in mem:
-        parent[b]=a
-    elif b in mem:
-        parent[a]=b
-
-    else:
-        if a<b:
-            parent[b]=a
-        else:
-            parent[a]=b
-
     
-n,m=map(int,input().split())
+    #a/b둘 중 한명이 진실을 아는 사람이면 하나로 묶
+    if ap in mem:
+        parent[bp]=ap
+    elif bp in mem:
+        parent[ap]=bp
+        
+    #둘 다 모르면 크기 비교후 묶
+    else:
+        if ap<bp:
+            parent[bp]=ap
+        else:
+            parent[ap]=bp
 
-b=list(map(int,input().split()))[1:]
-##아는 사람 
+
+n,m=map(int,input().split())
+truth=list(map(int,input().split()))[1:]
 
 g=[]
 parent=list(range(n+1))
@@ -37,19 +38,19 @@ parent=list(range(n+1))
 for i in range(m):
     temp=list(map(int,input().split()))
     people=temp[1:]
-    
+
     for j in range(temp[0]-1):
-        union(parent,people[j],people[j+1],b)
+        union(parent,people[j],people[j+1],truth)
     g.append(temp[1:])
-    
-    
+
 result=0
 
 for i in g:
     for j in range(len(i)):
-        if find(parent,i[j]) in b:
+        if find(parent,i[j]) in truth:
             break
+            ## 부모가 진실을 알고 있으면 break
     else:
-        result+=1
-
+         result+=1
 print(result)
+    
