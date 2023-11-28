eb15b6fe685aa9b0d6cb556e2fb0a515d0b3eb12
밍 <79096808+0321minji@users.noreply.github.com>
2023-11-28 13:46:33 +0900

    [Silver IV] Title: 궁합 쌍 찾기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14911. 궁합 쌍 찾기/궁합 쌍 찾기.py
+++ 백준/Silver/14911. 궁합 쌍 찾기/궁합 쌍 찾기.py
@@ -1,23 +1,22 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-input_list = list(map(int, input().split()))
-sum = int(input())
-output_list = []
-input_list.sort()
+innum=list(map(int,input().split()))
+goal=int(input())
 
-for i in range(len(input_list)-1):
-    for j in range(i+1, len(input_list)):
-        if input_list[i]+input_list[j]==sum:
-            if input_list[i] >= input_list[j]:
-                output_list.append((input_list[j], input_list[i]))
+result=[]
+innum.sort()
+
+for i in range(len(innum)-1):
+    for j in range(i+1,len(innum)):
+        if innum[i]+innum[j]==goal:
+            if innum[i]>=innum[j]:
+                result.append((innum[j],innum[i]))
             else:
-                output_list.append((input_list[i], input_list[j]))
+                result.append((innum[i],innum[j]))
+result.sort()
+result=list(dict.fromkeys(result))
 
-output_list.sort()
-output_list = list(dict.fromkeys(output_list)) # 리스트에서 기존 순서 유지하고 중복 제거하기
-
-for t in output_list:
-    print(t[0], end=' ')
-    print(t[1])
-print(len(output_list))
+for temp in result:
+    print(*temp)
+print(len(result))
