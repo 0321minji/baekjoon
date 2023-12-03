747b172d594c880c0e51deeb212f9e151a6ee526
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-03 21:26:34 +0900

    [Silver I] Title: 신입 사원, Time: 4496 ms, Memory: 50724 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,23 +1,16 @@
 import sys
 input=sys.stdin.readline
 
-def solve(score):
-    score.sort()
-    temp=score[0][1]
-    result=1
-    
-    for i in range(1,n):
-        if score[i][1]<temp:
-            result+=1
-            temp=score[i][1]    
-    
-    print(result)
-    
-
 t=int(input())
 for _ in range(t):
     score=[]
     n=int(input())
-    for _ in range(n):
-        score.append(list(map(int,input().split())))
-    solve(score)
+    score=[list(map(int,input().split())) for _ in range(n)]
+    score.sort()
+    temp=score[0][1]
+    result=1
+    for i in range(1,n):
+        if score[i][1]<temp:
+            result+=1
+            temp=score[i][1]
+    print(result)
