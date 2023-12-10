91a42454d3d87079f46f1ef2c8d5f968fba79b32
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-11 00:48:52 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 64 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,28 +1,17 @@
 import sys
+from collections import deque
 input=sys.stdin.readline
 
-n,k=map(int,input().split())
-people=[]
-
-for i in range(1,n+1):
-    people.append(i)
-
-result=[]
-index=k-1
-
-while people:
-    #k번째 index 제거
-    result.append(people.pop(index))
-    #index 값 갱신 (리스트 길이 감소->%연산)
-    if people:
-        index=(index+k-1)%len(people)
-
-print('<',end='')
-for i in range(n):
-    if i==n-1:
-        print(result[i],end='>')
-    else:
-        print(result[i],end=', ')
-
-
+def solve():
+    result='<'
+    index=0
+    while people:
+        index+=k-1
+        index=index%len(people)
+        result+=str(people.pop(index))+', '
+    result=result[:-2]+'>'
+    print(result)
     
+n,k=map(int,input().split())
+people=[i for i in range(1,n+1)]
+solve()
