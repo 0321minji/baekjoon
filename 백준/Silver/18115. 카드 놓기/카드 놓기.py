950db3caac4b602e40be39e00fb28d1a15eb8a01
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-11 00:33:13 +0900

    [Silver III] Title: 카드 놓기, Time: 748 ms, Memory: 94432 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.py
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.py
@@ -2,18 +2,19 @@ import sys
 from collections import deque
 input=sys.stdin.readline
 
+def solve(n,a):
+    card=1
+    result=deque([])
+    for i in range(n-1,-1,-1):
+        if a[i]==1:
+            result.appendleft(card)
+        elif a[i]==2:
+            result.insert(1,card)
+        else:
+            result.append(card)
+        card+=1
+    return result
+
 n=int(input())
-rule=list(map(int,input().split()))
-num=1
-result=deque()
-while rule:
-    t=rule.pop()
-    if t==1:
-        result.appendleft(num)
-    elif t==2:
-        result.insert(1,num)
-    elif t==3:
-        result.append(num)
-    num+=1
-print(*result)
-        
+a=list(map(int,input().split()))
+print(*solve(n,a))
