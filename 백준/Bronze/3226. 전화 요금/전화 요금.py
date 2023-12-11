01e560e68e13a6878e4214028ebcd6ce0829c671
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-11 20:21:07 +0900

    [Bronze I] Title: 전화 요금, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3226. 전화 요금/전화 요금.py
+++ 백준/Bronze/3226. 전화 요금/전화 요금.py
@@ -1,27 +1,33 @@
+import sys
+input=sys.stdin.readline
+
+def solve(h,m,d):
+    global result
+    if 7<=h<=17:
+        result+=d*10
+    elif 0<=h<=5 or 19<=h<24:
+        
+        result+=d*5
+    else:
+        if (m+d)//60:
+            t=(m+d)%60
+            if h==18:
+                result+=t*5
+                result+=(d-t)*10
+            else:
+                result+=t*10
+                result+=(d-t)*5
+        else:
+            if h==18:
+                result+=d*10
+            elif h==6:
+                result+=d*5
+            
+        
 
 n=int(input())
-charge=0
-for i in range(n):
-    line=input()
-    h=int(line[:2])
-    m=int(line[3:5])
-    d=int(line[6:])
-
-    end_h=h
-    end_m=(m+d)%60
-
-    if m+d>=60:
-        end_h+=1
-        if end_h+1==24:
-            end_h=0
-
-    if h==6 and end_h==7:
-        charge+=(d-end_m)*5 + end_m*10
-    elif h==18 and end_h==19:
-        charge+=(d-end_m)*10+end_m*5
-    else:
-        if 6<h<19:
-            charge+=10*d
-        else:
-            charge+=5*d
-print(charge)
+result=0
+for _ in range(n):
+    temp=list(map(str,input().split()))
+    solve(int(str(temp[0][:2])),int(str(temp[0][3:])),int(temp[1]))
+print(result)
