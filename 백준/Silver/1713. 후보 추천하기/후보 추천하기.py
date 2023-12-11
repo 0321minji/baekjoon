379186beb10777ecda6f2637cc47f6dec4998bba
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-12 02:40:16 +0900

    [Silver I] Title: 후보 추천하기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,30 @@
+import sys
+input=sys.stdin.readline
+
+def solve():
+    #0-추천수 1-시간
+    photo=[[10**3,10**3] for _ in range(101)]
+    photo[0]=[10**8,10**8]
+    stu=[]
+    for i in range(t):
+        if g[i] in stu:
+            photo[g[i]][0]+=1
+        elif len(stu)<n:
+            photo[g[i]]=[1,i]
+            stu.append(g[i])
+        else:
+            #추천 젤 작은거&젤 오래된거 삭제 
+            index=photo.index(min(photo,key=lambda x:(x[0],x[1])))
+            #print(index,photo)
+            photo[index]=[10**3,10**3]
+            stu.remove(index)
+            photo[g[i]]=[1,i]
+            stu.append(g[i])
+    stu.sort()
+    print(*stu)
+            
+            
+n=int(input())
+t=int(input())
+g=list(map(int,input().split()))
+solve()
