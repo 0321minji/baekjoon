1c974d60ce5fbbab7fa5ceaed3abe2b6d674ee2e
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-11 19:04:29 +0900

    [Gold III] Title: 오등큰수, Time: 1460 ms, Memory: 151800 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.py
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -2,22 +2,21 @@ import sys
 from collections import Counter
 input=sys.stdin.readline
 
+def solve():
+    cnt=Counter(a)
+    f=[cnt[i] for i in a]
+    result=[-1]*(n)
+    st=[0]
+    i=1
+    while st and i<n:
+        while st and f[st[-1]]<f[i]:
+            result[st[-1]]=a[i]
+            st.pop()
+        st.append(i)
+        i+=1
+    return result
+
 n=int(input())
-num=list(map(int,input().split()))
+a=list(map(int,input().split()))
+print(*solve())
 
-cnt=Counter(num)
-#등장횟수'
-f=[cnt[i] for i in num]
-m=max(f)
-result=[-1]*n
-
-st=[0]
-i=1
-while st and i<n:
-    while st and f[st[-1]]<f[i]:
-        result[st[-1]]=num[i]
-        st.pop()
-    st.append(i)
-    i+=1
-
-print(*result)
