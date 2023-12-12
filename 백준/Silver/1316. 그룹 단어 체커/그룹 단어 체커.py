aa8d11cdffa407380acb689a703661ac26720acc
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-12 15:13:34 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,17 +1,23 @@
 import sys
 input=sys.stdin.readline
 
-def solve(word):
+def solve(string):
     global result
     alp=[]
-    for i in word:
-        if i in alp and alp[-1]!=i:
-            return
-        alp.append(i)
+    for i in string:
+        if not i in alp:
+            alp.append(i)
+        else:
+            if alp[-1]!=i:
+                return
     result+=1
-    
+        
+
+
 n=int(input())
 result=0
 for _ in range(n):
-    solve(input().rstrip())
+    string=list(input().rstrip())
+    solve(string)
+
 print(result)
