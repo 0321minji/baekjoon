262186f4e660f424f238485d7b16979b72e84c9e
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-19 15:41:16 +0900

    [Silver II] Title: 외판원 순회 2, Time: 144 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,35 +1,31 @@
-import sys, math
+import sys
 input=sys.stdin.readline
 
-def dfs(st,now,v,level):
+def solve(start,now,w,level):
     global result
-    # level==n 이면 자기 자신으로 돌아가야함
     if level==n:
-        # [now][st]가 0 이면 잘못된 경
-        if w[now][st]:
-            v+=w[now][st]
-            if result>v:
-                result=v
+        if load[now][start]:
+            w+=load[now][start]
+            if result>w:
+                result=w
         return
-    
-    #갱신 필요 X
-    if v > result:
+
+    if w>result:
         return
 
     for i in range(n):
-        if not visit[i] and w[now][i]:
+        if not visit[i] and load[now][i]:
             visit[i]=1
-            dfs(st,i,v+w[now][i],level+1)
+            solve(start,i,w+load[now][i],level+1)
             visit[i]=0
-    
-    
 
 n=int(input())
-w=[list(map(int,input().split())) for _ in range(n)]
-result=10**8
+load=[list(map(int,input().split())) for _ in range(n)]
+result=10**7
 visit=[0]*n
+
 for i in range(n):
     visit[i]=1
-    dfs(i,i,0,1)
+    solve(i,i,0,1)
     visit[i]=0
 print(result)
