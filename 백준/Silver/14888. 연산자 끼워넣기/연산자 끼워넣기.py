09e12c40bed5ca199110ad7ef3a1802905f6949c
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-24 21:36:49 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,17 +1,16 @@
 import sys
 input=sys.stdin.readline
 
-n=int(input())
-a=list(map(int,input().split()))
-cal=list(map(int,input().split()))
-
 def solve(i,temp,add,minus,mul,div):
     global MAX, MIN
+
     if i==n:
-        MAX=max(temp,MAX)
-        MIN=min(temp,MIN)
+        if temp>MAX:
+            MAX=temp
+        if temp<MIN:
+            MIN=temp
         return
-    
+
     if add:
         solve(i+1,temp+a[i],add-1,minus,mul,div)
     if minus:
@@ -20,9 +19,16 @@ def solve(i,temp,add,minus,mul,div):
         solve(i+1,temp*a[i],add,minus,mul-1,div)
     if div:
         solve(i+1,int(temp/a[i]),add,minus,mul,div-1)
+        
+
+
+n=int(input())
+a=list(map(int,input().split()))
+cal=list(map(int,input().split()))
 
 MAX=-10**9
 MIN=10**9
+
 solve(1,a[0],cal[0],cal[1],cal[2],cal[3])
 print(MAX)
 print(MIN)
