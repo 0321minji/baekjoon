bcaeecb6d6c27490171d59f07155927dcfe96a77
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-24 21:27:31 +0900

    [Silver II] Title: 꽃길, Time: 476 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.py
+++ 백준/Silver/14620. 꽃길/꽃길.py
@@ -1,41 +1,38 @@
 import sys
 input=sys.stdin.readline
 
-move=[[0,1],[0,-1],[-1,0],[1,0]]
-
-n=int(input())
-money=[list(map(int,input().split())) for _ in range(n)]
-visit=[]
-
 def check(visit,x,y):
     for a,b in move:
-        nx=x+a;
-        ny=y+b
+        nx=x+a; ny=y+b
         if [nx,ny] in visit:
             return False
     return True
 
-result=10**8
 def solve(visit,total):
     global result
-    if total>=result:
-        #result 값보다 total이 더 큰 경우 무시
-        return
-    if len(visit)==15:##5*3 (3개의 씨앗을 다 심은 경우
-        result=min(total,result)
 
+    if total>=result:
+        return
+    if len(visit)==15:
+        result=min(result,total)
+        return
     else:
-        for i in range(1,n-1):
-            for j in range(1,n-1): #어차피 맨 가장자리에 씨앗 심을 수 X
-                if check(visit,i,j) and (i,j) not in visit: #방문체크
-                    #씨앗 심을 위치 = temp
+        for i in range(1,n-1): ##가장 끝 줄은 어차피 못심음
+            for j in range(1,n-1):
+                if check(visit,i,j) and (i,j) not in visit:
                     temp=[[i,j]]
-                    temp_cost=money[i][j]
+                    temp_cost=cost[i][j]
                     for a,b in move:
                         nx=i+a; ny=j+b
                         temp.append([nx,ny])
-                        temp_cost+=money[nx][ny]
-                    #해당 위치를 기준으로 5칸 값 탐색 => solve 호출해서 비교
+                        temp_cost+=cost[nx][ny]
                     solve(visit+temp,total+temp_cost)
+    
+n=int(input())
+move=[[0,1],[0,-1],[1,0],[-1,0]]
+cost=[list(map(int,input().split())) for _ in range(n)]
+visit=[]
+result=10**8
+
 solve(visit,0)
 print(result)
