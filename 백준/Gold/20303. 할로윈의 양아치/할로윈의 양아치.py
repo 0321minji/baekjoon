be75a204262795d014d160ac82bd9f51b9bedb4f
밍 <79096808+0321minji@users.noreply.github.com>
2023-12-27 22:46:06 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 956 ms, Memory: 120580 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
@@ -1,42 +1,47 @@
 import sys
-from collections import deque, defaultdict
+from collections import deque
 input=sys.stdin.readline
 
+def solve():
+    global temp
+    visit=[0]*(n+1)
+    for i in range(1,n+1):
+        if not visit[i]:
+            visit[i]=1
+            que=deque([i])
+            count,result=0,0
+            while que:
+                node=que.popleft()
+                count+=1
+                result+=c[node]
+                for to in graph[node]:
+                    if not visit[to]:
+                        visit[to]=1
+                        que.append(to)
+            temp.append((count,result))
+    return dynamic()
+    
+    
+def dynamic():
+    dp=[[0]*(k+1) for _ in range(2)]
+    for count,result in temp:
+        for i in range(0,k+1):
+            if i>=count:
+                dp[1][i]=max(dp[0][i],dp[0][i-count]+result)
+            else:
+                dp[1][i]=dp[0][i]
+        for i in range(0,k+1):
+            dp[0][i]=dp[1][i]
+    #print(dp)
+    return dp[1][k-1]
+
 n,m,k=map(int,input().split())
-candy=list(map(int,input().split()))
-candy.insert(0,0)
-f=defaultdict(list)
-
-dp=[[0]*(k+1) for _ in range(2)]
-visit=[True]*(n+1)
-table=[]
-
-for i in range(m):
+c=list(map(int,input().split()))
+c.insert(0,0)
+graph=[[] for _ in range(n+1)]
+temp=[]
+for _ in range(m):
     a,b=map(int,input().split())
-    f[a].append(b)
-    f[b].append(a)
-
-for i in range(1,n+1):
-    if visit[i]:
-        visit[i]=False
-        que=deque([i])
-        count,result=0,0
-        while(que):
-            node=que.popleft()
-            count+=1
-            result+=candy[node]
-            for to in f[node]:
-                if visit[to]:
-                    visit[to]=False
-                    que.append(to)
-        table.append((count,result))
-for count,result in table:
-    for i in range(0,k+1):
-        if i>=count:
-            dp[1][i]=max(dp[0][i],dp[0][i-count]+result)
-        else:
-            dp[1][i]=dp[0][i]
-    for i in range(0,k+1):
-        dp[0][i]=dp[1][i]
-
-print(dp[1][k-1])
+    graph[a].append(b)
+    graph[b].append(a)
+print(solve())
