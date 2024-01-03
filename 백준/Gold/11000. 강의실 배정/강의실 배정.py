309fb7456318b13798e1896038b0e45f1a549bd6
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-03 22:10:31 +0900

    [Gold V] Title: 강의실 배정, Time: 392 ms, Memory: 73448 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,25 +1,20 @@
-import sys
+import sys, heapq
 input=sys.stdin.readline
-import heapq
 
 n=int(input())
-
-time=[]
-
-for i in range(n):
-    inn=list(map(int,input().split()))
-    time.append(inn)
-    
-time.sort(key=lambda x:(x[0],x[1]))
+time=[list(map(int,input().split())) for _ in range(n)]
+time.sort()
 
 que=[]
 heapq.heappush(que,time[0][1])
 
 for i in range(1,n):
-    if que[0] > time[i][0]:
+    #끝나는 시간 넣기
+    #끝나는 시간이 더 늦으면 강의실 추가
+    if que[0]> time[i][0]:
         heapq.heappush(que,time[i][1])
+    #아니라면 수업 교체(끝나는 시간 갱신)
     else:
         heapq.heappop(que)
         heapq.heappush(que,time[i][1])
-
 print(len(que))
