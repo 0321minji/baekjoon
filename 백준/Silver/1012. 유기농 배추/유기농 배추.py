e570c281062cf4746706caff921cbc26c2cb58e0
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-05 21:16:16 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,31 +1,32 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
 move=[[0,1],[0,-1],[1,0],[-1,0]]
-
-def check(a,b):
-    st=[[a,b]]
-    while st:
-        x,y=st.pop()
-        for a,b in move:
-            nx=x+a; ny=y+b
-            if 0<=nx<m and 0<=ny<n and ground[nx][ny]:
-                st.append([nx,ny])
-                ground[nx][ny]=0
-
+def dfs(a,b):
+    que=[(a,b)]
+    while que:
+        a,b=que.pop()
+        for dx,dy in move:
+            nx=a+dx; ny=b+dy
+            if 0<=nx<m and 0<=ny<n and graph[nx][ny]==1:
+                que.append((nx,ny))
+                graph[nx][ny]=2
 
 t=int(input())
 for _ in range(t):
-    snail=0
     m,n,k=map(int,input().split())
-    ground=[[0]*(n) for _ in range(m)]
+    graph=[[0]*(n) for _ in range(m)]
+    result=0
+    
     for _ in range(k):
         x,y=map(int,input().split())
-        ground[x][y]=1
-
+        graph[x][y]=1
     for i in range(m):
         for j in range(n):
-            if ground[i][j]:
-                check(i,j)
-                snail+=1
-    print(snail)
+            if graph[i][j]==1:
+                graph[i][j]=2
+                result+=1
+                dfs(i,j)
+                #graph[i][j]=1
+    print(result)
+        
