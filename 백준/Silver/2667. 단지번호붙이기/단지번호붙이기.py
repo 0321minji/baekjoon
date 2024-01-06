b1670635f461590e49148dd98b9a7831057fc17e
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-06 21:39:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,34 +1,32 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
 move=[[0,1],[0,-1],[1,0],[-1,0]]
 
-def solve(a,b,k):
-    st=[[a,b]]
-    count=0
-    while st:
-        x,y=st.pop()
-        for a,b in move:
-            nx=x+a; ny=y+b;
-            if 0<=nx<n and 0<=ny<n and apart[nx][ny]==k:
-                st.append([nx,ny])
-                apart[nx][ny]=0
-                count+=1
-                
-    return count
-    
+def bfs(x,y,cnt):
+    que=[(x,y)]
+
+    while que:
+        x,y=que.pop()
+        for dx,dy in move:
+            nx=x+dx; ny=y+dy
+            if 0<=nx<n and 0<=ny<n and graph[nx][ny]:
+                    que.append((nx,ny))
+                    graph[nx][ny]=0
+                    cnt+=1
+                    
+    return cnt
+
 n=int(input())
-
-apart=[list(map(int,list(input().rstrip()))) for _ in range(n)]
-
+graph=[list(map(int,list(input().rstrip()))) for _ in range(n)]
 result=[]
+
 for i in range(n):
     for j in range(n):
-        if apart[i][j]:
-            temp=solve(i,j,apart[i][j])
-            if not temp:
-                result.append(1)
-            else:
-                result.append(temp)
+        if graph[i][j]:
+            graph[i][j]=0
+            result.append(bfs(i,j,1))
+
+print(len(result))
 result.sort()
-print(len(result),*result,sep='\n')
+print(*result,sep='\n')
