20fc172879e0cc4f53d3671308956a7dca2bdd2b
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-06 23:34:55 +0900

    [Silver I] Title: 안전 영역, Time: 816 ms, Memory: 31748 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,36 +1,41 @@
-import sys
-input=sys.stdin.readline
+import sys, copy
+input = sys.stdin.readline
 
 move=[[0,1],[0,-1],[1,0],[-1,0]]
 
-def solve(a,b,k,visit):
-    st=[[a,b]]
-    while st:
-        x,y=st.pop()
-        for a,b in move:
-            nx=x+a; ny=y+b;
-            if 0<=nx<n and 0<=ny<n and h[nx][ny]>k and not visit[nx][ny]:
-                st.append([nx,ny])
-                visit[nx][ny]=1
+def bfs(h,i,j,graph):
+    que=[(i,j)]
+    while que:
+        x,y=que.pop()
+        for dx,dy in move:
+            nx=x+dx; ny=y+dy
+            if 0<=nx<n and 0<=ny<n and graph[nx][ny]>h:
+                que.append((nx,ny))
+                graph[nx][ny]=0
                 
-
-    
-n=int(input())
-h=[list(map(int,input().split())) for _ in range(n)]
-
-result=0
-height=max(map(max,h))
-
-for k in range(height):
-    temp=0
-    visit=[[0]*n for _ in range(n)]
+            
+def solve(h,graph):
+    result=0
     for i in range(n):
         for j in range(n):
-            if h[i][j]>k and not visit[i][j]:
-                solve(i,j,k,visit)
-                temp+=1
-                visit[i][j]=1
+            if graph[i][j]>h:
+                result+=1
+                graph[i][j]=0
+                bfs(h,i,j,graph)
 
-    result=max(temp,result)
+                
+    return result
+    
 
+n=int(input())
+rain=[list(map(int,input().split())) for _ in range(n)]
+
+high=max(map(max,rain))
+result=0
+
+for h in range(high):
+    #result=max(result,solve(h,rain))
+    temp=copy.deepcopy(rain)
+    result=max(result,solve(h,temp))
+    
 print(result)
