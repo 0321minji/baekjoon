86c2213f27bba7423b9b1795b7eb9c73c4e3d5ee
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-07 22:32:09 +0900

    [Gold IV] Title: 루머, Time: 1740 ms, Memory: 180584 KB -BaekjoonHub

--- 백준/Gold/19538. 루머/루머.py
+++ 백준/Gold/19538. 루머/루머.py
@@ -2,42 +2,40 @@ import sys
 from collections import deque
 input=sys.stdin.readline
 
+move=[[0,1],[0,-1],[1,0],[-1,0]]
+
 def solve():
-    que=deque([])
-    while rumor:
-        index=rumor.popleft()
-        temp=people[index]
-        for i in temp:
-            if time[i]!=-1:
+    que=deque(rumor)
+    while que:
+        index=que.popleft()
+        fr=people[index]
+        for i in fr:
+            if i==-1:
                 continue
-            count=0
-            for k in range(len(people[i])):
-                if time[people[i][k]]!=-1:
-                    count+=1
-            if len(people[i])>count*2:
+            if time[i]>=0:
                 continue
-            que.append(i)
-        #해당 시간에 대한 탐색이 끝
-        if not rumor:
-            while que:
-                tmp=que.popleft()
-                time[tmp]=time[index]+1
-                rumor.append(tmp)
-    print(*time[1:])
-
+            if index in people[i]:
+                people[i].remove(index)
+                people[i].append(-1)
+            #print(i,people[i],people[i].count(-1),len(people[i]))
+            
+            if people[i].count(-1)>=((len(people[i])+1)//2):
+                time[i]=time[index]+1
+             #   print(time,i,index)
+                que.append(i)
+            
 n=int(input())
-people=[]
-for _ in range(n):
+people=[[] for _ in range(n+1)]
+for i in range(1,n+1):
     temp=list(map(int,input().split()))
-    people.append(temp[:-1])
+    people[i]=temp[:-1]
 
-people.insert(0,[])
 m=int(input())
 rumor=list(map(int,input().split()))
-
 time=[-1]*(n+1)
+
 for i in rumor:
     time[i]=0
 
-rumor=deque(rumor)
 solve()
+print(*time[1:])
