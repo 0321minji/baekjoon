89f0a96f150ffca95fbcd3b4f24bff4c04e5d03b
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-07 21:35:00 +0900

    [Silver II] Title: 유기농 배추, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,32 +1,33 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
 move=[[0,1],[0,-1],[1,0],[-1,0]]
-def dfs(a,b):
-    que=[(a,b)]
+
+def solve(x,y):
+    que=[(x,y)]
     while que:
-        a,b=que.pop()
+        x,y=que.pop()
         for dx,dy in move:
-            nx=a+dx; ny=b+dy
-            if 0<=nx<m and 0<=ny<n and graph[nx][ny]==1:
+            nx=x+dx; ny=y+dy
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny]:
+                graph[nx][ny]=0
                 que.append((nx,ny))
-                graph[nx][ny]=2
+                
 
 t=int(input())
 for _ in range(t):
     m,n,k=map(int,input().split())
-    graph=[[0]*(n) for _ in range(m)]
+    graph=[[0]*(m) for _ in range(n)]
+    #print(graph)
     result=0
-    
     for _ in range(k):
         x,y=map(int,input().split())
-        graph[x][y]=1
-    for i in range(m):
-        for j in range(n):
-            if graph[i][j]==1:
-                graph[i][j]=2
-                result+=1
-                dfs(i,j)
-                #graph[i][j]=1
-    print(result)
         
+        graph[y][x]=1
+    for i in  range(n):
+        for j in range(m):
+            if graph[i][j]:
+                graph[i][j]=0
+                solve(i,j)
+                result+=1
+    print(result)
