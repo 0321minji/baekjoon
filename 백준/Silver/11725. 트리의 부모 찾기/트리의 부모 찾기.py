d1b8f70e0cb33495b4242a336c1f95da4e3aca3e
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-07 21:49:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 320 ms, Memory: 66492 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,23 +1,24 @@
 import sys
 input=sys.stdin.readline
 sys.setrecursionlimit(10**6)
-
-def solve(tree,s,p):
-    for i in tree[s]:
-        if p[i]==0:
-            p[i]=s
-            solve(tree,i,p)
-
-
+def solve(index,parent):
+    for i in tree[index]:
+        if not parent[i]:
+            parent[i]=index
+            solve(i,parent)
+    
+    
+    
 n=int(input())
 tree=[[] for _ in range(n+1)]
 
-for i in range(n-1):
+for _ in range(n-1):
     a,b=map(int,input().split())
     tree[a].append(b)
     tree[b].append(a)
 
-p=[0]*(n+1)
-solve(tree,1,p)
+parent=[0]*(n+1)
+parent[1]=1
+solve(1,parent)
+print(*parent[2:],sep='\n')
 
-print(*p[2:],sep='\n')
