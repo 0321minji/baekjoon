1fc76f9283aa28079bfbd5ad84f6bfe3e3fd18ad
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-09 15:00:10 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,18 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+
 n=int(input())
-g=[]
-for i in range(n):
-    g.append(int(input()))
+m=[int(input().rstrip()) for _ in range(n)]
 
-dp=[0]*(n)
-dp[0]=g[0]
-
+dp=[0]*n
+dp[0]=m[0]
 
 for i in range(1,n):
     if i<2:
-        dp[1]=g[0]+g[1]
+        dp[i]=m[0]+m[1]
     elif i<3:
-        dp[2]=max(dp[1],dp[0]+g[2],g[1]+g[2])
+        dp[i]=max(dp[1],dp[0]+m[2],m[1]+m[2])
     else:
-        dp[i]=max(dp[i-3]+g[i-1]+g[i],dp[i-2]+g[i],dp[i-1])
-    
+        dp[i]=max(dp[i-3]+m[i-1]+m[i],dp[i-2]+m[i],dp[i-1])
+
 print(dp[n-1])
