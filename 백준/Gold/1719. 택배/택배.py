109889a16c9bde660589d4e2f2235f803e5e0ad0
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-15 22:15:34 +0900

    [Gold III] Title: 택배, Time: 776 ms, Memory: 33216 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,44 +1,46 @@
-import sys, heapq
+import sys , heapq
 input=sys.stdin.readline
 
-def dijk(start, n):
-    global load, result
+def solve(start,n):
+    global graph, hub
+    
     hq=[]
     for i in range(1,n+1):
         if i==start:
             continue
-        heapq.heappush(hq,(load[start][i],i))
+        heapq.heappush(hq,(graph[start][i],i))
 
     while hq:
-        dist, node= heapq.heappop(hq)
-        if load[start][node]<dist:
+        dist,node=heapq.heappop(hq)
+        if graph[start][node]<dist:
             continue
         for nei in range(1,n+1):
-            if node==nei or start == nei:
-                #자기 자신
+            #자기 자신 pass
+            if node==nei or start==nei:
                 continue
-            if dist+ load[node][nei] < load[start][nei]:
-                load[start][nei]= dist+load[node][nei]
-                heapq.heappush(hq, (dist+load[node][nei], nei))
-                result[start][nei]=result[start][node]
-    
-            
+            #node를 거치는 것이 start-nei 바로 가는 것보다  작다면 갱신
+            if dist+graph[node][nei] < graph[start][nei]:
+                graph[start][nei]=dist+graph[node][nei]
+                heapq.heappush(hq,(graph[start][nei],nei))
+                #***** 집하장도 갱신!!! ******* 
+                hub[start][nei]=hub[start][node]
+                
 
-inf=10**8
+
 n,m=map(int,input().split())
-load=[[inf]*(n+1) for _ in range(n+1)]
-result=[['-']*(n+1) for _ in range(n+1)]
+#최단거리 값이랑 그때의 집하장 번호를 저장하는 리스트 따로따로
+graph=[[10**8]*(n+1) for _ in range(n+1)]
+hub=[['-']*(n+1) for _ in range(n+1)]
 
 for _ in range(m):
     a,b,t=map(int,input().split())
-    load[a][b]=load[b][a]=t
-    result[a][b]=b
-    result[b][a]=a
+    graph[a][b]=graph[b][a]=t
+    hub[a][b]=b
+    hub[b][a]=a
 
 for i in range(1,n+1):
-    dijk(i,n)
+    #i 정점으로부터 각 정점까지의 최단거리 구함
+    solve(i,n)
 
-for res in result[1:]:
-    print(*res[1:])
-    
-   
+for i in range(1,n+1):
+    print(*hub[i][1:])
