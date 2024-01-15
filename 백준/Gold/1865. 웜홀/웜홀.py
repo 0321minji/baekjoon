c6d8d851fde511dd6645a3abaadf69322e8f46ef
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-15 21:51:19 +0900

    [Gold III] Title: 웜홀, Time: 1260 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,39 +1,28 @@
-import sys
+import sys, heapq
 input=sys.stdin.readline
 
-t=int(input())
-def solve(n):
-    global check
-
+def solve():
+    dist=[10**8]*(n+1)
     for i in range(1,n+1):
         for j in range(1,n+1):
-            for e,wei in road[j]:
-                if(dist[e]>wei+dist[j]):
-                    dist[e]=wei+dist[j]
-                    if(i==n):
-                        check=1
-    
-for i in range(t):
+            for e,wei in load[j]:
+                if dist[e]>dist[j]+wei:
+                    dist[e]=dist[j]+wei
+                    if i==n:
+                        return 'YES'
+    return 'NO'
+
+
+tc=int(input())
+for _ in range(tc):
     n,m,w=map(int,input().split())
-    road=[[] for _ in range(n+1)]
-    inf=10**9
-    dist=[inf]*(n+1)
-
-
-    for i in range(m):
+    load=[[] for _ in range(n+1)]
+    for _ in range(m):
         s,e,t=map(int,input().split())
-        road[s].append((e,t)) #t 증가하는 시간
-        road[e].append((s,t)) # 양방향 그래프라서
-    for i in range(w):
+        load[s].append([e,t])
+        load[e].append([s,t])
+    for _ in range(w):
         s,e,t=map(int,input().split())
-        road[s].append((e,-t)) #이때 t 줄어드는 시간
+        load[s].append([e,-t])
 
-    check=0
-
-    solve(n)
-
-    if check==1:
-        print('YES')
-
-    else:
-        print('NO')
+    print(solve())
