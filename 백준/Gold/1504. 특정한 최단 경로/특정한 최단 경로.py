a975e322a45121ef2fd30b8e2e1cd6c40ece9a16
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-15 21:44:28 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 1024 ms, Memory: 69360 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,37 +1,38 @@
-#1->v1->v2->n or 1->v2->v1->n 둘 중 최소
-
 import sys, heapq
 input=sys.stdin.readline
 
-def dij(start):
-    dist=[10**9]*(n+1)
+def get(start):
+    #start 정점으로부터의 길이
+    dist=[10**8]*(n+1)
     dist[start]=0
+
     pq=[]
     heapq.heappush(pq,(0,start))
 
     while pq:
-        w, end= heapq.heappop(pq)
-        for nxt, nxt_w in graph[end]:
+        w, e = heapq.heappop(pq)
+        for nxt,nxt_w in graph[e]:
             wei=nxt_w+w
             if dist[nxt]>wei:
                 dist[nxt]=wei
                 heapq.heappush(pq,(wei,nxt))
-
     return dist
     
 
 n,e=map(int,input().split())
-graph=[[]for _ in range(n+1)]
-
+graph=[[] for _ in range(n+1)]
 for _ in range(e):
     a,b,c=map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
+    graph[a].append([b,c])
+    graph[b].append([a,c])
 
 v1,v2=map(int,input().split())
 
-dis1=dij(v1)
-dis2=dij(v2)
+v1d=get(v1)
+v2d=get(v2)
 
-result=min(dis1[1]+dis1[v2]+dis2[n],dis2[1]+dis2[v1]+dis1[n])
-print(result if result<10**9 else -1)
+#1->v1->v2->n or 1->v2->v1->n
+result=min(v1d[1]+v1d[v2]+v2d[n], v2d[1]+v2d[v1]+v1d[n])
+
+print(result if result<10**8 else -1)
+
