7af53dc5faba694742e64cfa25e77f25d84eba1c
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-15 21:23:39 +0900

    [Gold IV] Title: 해킹, Time: 1212 ms, Memory: 53120 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -2,12 +2,10 @@ import sys, heapq
 input=sys.stdin.readline
 
 t=int(input())
-inf=10**8
-
 for _ in range(t):
     n,d,c=map(int,input().split())
     com=[[] for _ in range(n+1)]
-    visit=[inf]*(n+1)
+    visit=[10**8]*(n+1)
     for _ in range(d):
         a,b,s=map(int,input().split())
         com[b].append([a,s])
@@ -16,6 +14,7 @@ for _ in range(t):
     heapq.heappush(pq,[0,c])
     cnt=0
     time=0
+
     while pq:
         t,cn=heapq.heappop(pq)
         if visit[cn]<=t:
@@ -23,7 +22,8 @@ for _ in range(t):
         visit[cn]=t
         cnt+=1
         time=t
-        for nc, nt in com[cn]:
+
+        for nc,nt in com[cn]:
             if visit[nc]>nt+t:
                 heapq.heappush(pq,[nt+t,nc])
     print(cnt,time)
