467f9ce12830641d8f06cb9151f83cf696f19764
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-15 21:30:43 +0900

    [level Lv.3] Title: 합승 택시 요금, Time: 286.10 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/Lv.3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/Lv.3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,6 +1,6 @@
-#그래프 초기화
 def fill(n,fares):
-    graph=[[10**8]*(n+1) for _ in range(n+1)]
+    inf=10**8
+    graph=[[inf]*(n+1) for _ in range(n+1)]
     for i in range(1,n+1):
         graph[i][i]=0
     for a,b,d in fares:
@@ -8,7 +8,6 @@ def fill(n,fares):
         graph[b][a]=d
     return graph
 
-##dp랑 비슷...???
 def solution(n, s, a, b, fares):
     answer = 10**8
     graph=fill(n,fares)
@@ -17,8 +16,10 @@ def solution(n, s, a, b, fares):
         for i in range(1,n+1):
             for j in range(1,n+1):
                 graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j])
+    
     for i in range(1,n+1):
         answer=min(answer,graph[s][i]+graph[i][a]+graph[i][b])
-
+    
+    
     
     return answer
