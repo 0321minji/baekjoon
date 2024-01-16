7d5585ce7ad967e62a8df5e0b45dadd8d420e9d9
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-16 22:52:39 +0900

    [Gold IV] Title: 트리의 지름, Time: 160 ms, Memory: 114672 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,35 +1,33 @@
 import sys
-from collections import deque
 input=sys.stdin.readline
+from collections import deque
 
-def solve(p,node):
-    q=deque([])
-    q.append(p)
+def solve(p,t):
+    que=deque([])
+    que.append(p)
     visit=[-1]*(n+1)
     visit[p]=0
 
-    while q:
-        x=q.popleft()
-        for nxt, w in tree[x]:
+    while que:
+        x=que.popleft()
+        for nxt, wei in tree[x]:
             if visit[nxt]==-1:
-                visit[nxt]=visit[x]+w
-                q.append(nxt)
-    #print(visit)
-    if node==1:
+                visit[nxt]=visit[x]+wei
+                que.append(nxt)
+    
+    if t==1:
         return visit.index(max(visit))
-    else:
-        return max(visit)
-        
+    return max(visit)
+
 n=int(input())
 tree=[[] for _ in range(n+1)]
-result=0
-visit=[0]*(n+1)
+
 for _ in range(n-1):
     a,b,c=map(int,input().split())
-    tree[a].append([b,c])
-    tree[b].append([a,c])
+    tree[a].append((b,c))
+    tree[b].append((a,c))
 
-#1번 노드로부터 가장 큰 가중치를 가진 노드
+#1로부터 가장 먼 거리에 있는 (가장 큰 가중치의) 정점
 index=solve(1,1)
-#해당 노드로부터 다시 탐색 => 가장 긴 지름
+#해당 정점으로 부터 가장 먼 거리에 있는~>.
 print(solve(index,2))
