5851abaf5621fd25aaf2d634e11bce774c740e46
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-16 22:58:01 +0900

    [level Lv.2] Title: 게임 맵 최단거리, Time: 5.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/Lv.2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,21 @@
 from collections import deque
 move=[[0,1],[0,-1],[1,0],[-1,0]]
-def check(i,j,n,m,maps):
-    que=deque([(i,j)])
+
+def solve(i,j,maps):
+    que=deque([])
+    que.append((i,j))
+    
     while que:
         x,y=que.popleft()
         for dx,dy in move:
             nx=x+dx; ny=y+dy
-            if 0<=nx<n and 0<=ny<m and maps[nx][ny]==1:
+            if 0<=nx<len(maps) and 0<=ny<len(maps[0]) and maps[nx][ny]==1:
                 maps[nx][ny]=maps[x][y]+1
                 que.append((nx,ny))
-    return maps[n-1][m-1]  
-
+    return maps[len(maps)-1][len(maps[0])-1]
+        
 
 def solution(maps):
-    answer = 10**8
-    n=len(maps)
-    m=len(maps[0])
-
-    temp=check(0,0,n,m,maps)
-    if temp!=-1:
-        answer=min(answer,temp)
-    if answer==1:
-        answer=-1
-    return answer
+    answer = solve(0,0,maps)
+    
+    return answer if answer!=1 else -1
