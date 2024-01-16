bb4ba30d65062b8fb9db6e553e7feeba5a8151a4
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-16 23:04:59 +0900

    [level Lv.3] Title: 네트워크, Time: 0.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43162. 네트워크/네트워크.py
+++ 프로그래머스/Lv.3/43162. 네트워크/네트워크.py
@@ -1,22 +1,23 @@
 from collections import deque
-def check(node , visit, computers,n):
-    q=deque([node])
+def solve(n,i,visit,computers):
+    visit[i]=1
+    que=deque([])
+    que.append(i)
     
-    while q:
-        temp=q.popleft()
-        for i in range(n):
-            if temp!=i and computers[temp][i] and not visit[i]:
-                visit[i]=1
-                q.append(i)
-            
-
+    while que:
+        temp=que.popleft()
+        visit[temp]=1
+        for j in range(n):
+            if j!=temp and computers[temp][j]==1 and not visit[j]:
+                visit[j]=1
+                que.append(j)
+    
 def solution(n, computers):
     answer = 0
     visit=[0]*(n)
     for i in range(n):
         if not visit[i]:
-            visit[i]=1
-            check(i,visit,computers,n)
+            solve(n,i,visit,computers)
             answer+=1
     
     return answer
