6c28976c9874b710dbba9b58d2e266a6eb28bb14
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-16 23:21:00 +0900

    [level Lv.2] Title: 전력망을 둘로 나누기, Time: 2.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/Lv.2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,30 +1,33 @@
-def makeTree(c):
-    global parent, tree
-    for node in connect[c]:
-        if node != parent[c]:
-            tree[c].append(node)
-            parent[node]=c
-            makeTree(node)
-
-def countNode(c):
-    for node in tree[c]:
-        countNode(node)
-        size[c]+=size[node]
+from collections import deque
+def solve(tree,visit,r):
+    que=deque([r])
+    visit[r]=1
+    cnt=1
+    while que:
+        x=que.popleft()
+        for i in tree[x]:
+            if visit[i]==0:
+                visit[i]=1
+                que.append(i)
+                cnt+=1
+    return cnt
 
 def solution(n, wires):
-    global connect, parent,tree,size
-    parent=[-1]*(n+1)
-    size=[1]*(n+1)
-    connect=[[] for _ in range(n+1)]
+    answer = n
     tree=[[] for _ in range(n+1)]
-    answer = 100
     for a,b in wires:
-        connect[a].append(b)
-        connect[b].append(a)
-    makeTree(1)
-    countNode(1)
-    for i in range(1,n+1):
-        temp=abs((n-size[i])-size[i])
-        answer=min(answer,temp)
-    return answer
+        tree[a].append(b)
+        tree[b].append(a)
+    
+    for a,b in wires:
+        visit=[0]*(n+1)
 
+        tree[a].remove(b)
+        tree[b].remove(a)
+        
+        answer=min(abs(solve(tree,visit,a)-solve(tree,visit,b)),answer)
+        
+        tree[a].append(b)
+        tree[b].append(a)
+        
+    return answer
