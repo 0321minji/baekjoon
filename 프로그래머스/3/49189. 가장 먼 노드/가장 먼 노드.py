95006919a3adb9087c125a297ff37ed95d8b9858
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-24 10:55:53 +0900

    [level 3] Title: 가장 먼 노드, Time: 31.15 ms, Memory: 20 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def solution(n, edge):
+    answer = 0
+    
+    graph=[[] for _ in range(n+1)]
+    dist=[-1]*(n+1)
+    
+    for a,b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    def bfs():
+        q=deque([1])
+        dist[1]=0
+        
+        while q:
+            temp=q.popleft()
+            for i in graph[temp]:
+                if dist[i]==-1:
+                    q.append(i)
+                    dist[i]=dist[temp]+1
+    bfs()
+    long=max(dist)
+        
+    return dist.count(long)
