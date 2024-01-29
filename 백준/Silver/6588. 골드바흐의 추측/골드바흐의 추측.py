888c900b7f6ae550487bf209573fd6ad596c201d
밍 <79096808+0321minji@users.noreply.github.com>
2024-01-29 19:59:58 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 404 ms, Memory: 119240 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,25 @@
+import sys, math
+input=sys.stdin.readline
+def prime():
+    for i in range(2,int(math.sqrt(10**6))+1):
+        if number[i]:
+            for j in range(2*i,10**6+1,i):
+                number[j]=0
+
+
+number=[1]*(10**6+1)
+prime()
+
+while True:
+    n=int(input())
+    if not n:
+        break
+    
+    for i in range(n-3,2,-2):
+        if number[i] and number[n-i]:
+            print(f"{n} = {n-i} + {i}") 
+            break
+        
+    else:
+        print("Goldbach's conjecture is wrong.")
+ 
