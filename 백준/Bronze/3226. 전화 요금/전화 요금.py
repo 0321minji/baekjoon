ef2e76a440e906db51bfb2e72d5a5a41b56ec9df
밍 <79096808+0321minji@users.noreply.github.com>
2024-02-12 15:53:36 +0900

    [Bronze I] Title: 전화 요금, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3226. 전화 요금/전화 요금.py
+++ 백준/Bronze/3226. 전화 요금/전화 요금.py
@@ -2,29 +2,29 @@ import sys
 input=sys.stdin.readline
 
 def solve(h,m,d):
-    global result
+    if 7<=h<=17:
+        return d*10
+    if 0<=h<=5 or 19<=h<=23:
+        return d*5
     if h==6 or h==18:
-        if 60-m>d:
+        if m+d>60:
+            temp=(m+d)%60
             if h==6:
-                result+=d*5
+                return (d-temp)*5 + temp*10
             else:
-                result+=d*10
-        else:
-            if h==6:
-                result+=(60-m)*5
-                result+=((m+d)%60)*10
-            else:
-                result+=(60-m)*10
-                result+=((m+d)%60)*5
-    elif 7<=h<=17:
-        result+=d*10
-    else:
-        result+=d*5
-    return
-result=0
+                return temp*5 + (d-temp)*10
+        elif h==6:
+            return d*5
+        return d*10
+        
 n=int(input())
+result=0
 for _ in range(n):
-    call=list(map(str,input().split()))
-    solve(int(str(call[0][:2])),int(str(call[0][3:])),int(call[1]))
+    time,d=list(map(str,input().split()))
+    h=int(time[:2])
+    m=int(time[3:])
+    d=int(d)
+    tmp=solve(h,m,d)
     
+    result+=tmp
 print(result)
