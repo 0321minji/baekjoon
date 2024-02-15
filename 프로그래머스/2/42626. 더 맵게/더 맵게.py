946b116cfb0671665e88540fcd105a3016026faa
밍 <79096808+0321minji@users.noreply.github.com>
2024-02-15 22:13:08 +0900

    [level 2] Title: 더 맵게, Time: 2361.18 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,25 +1,20 @@
 import heapq
-
 def solution(scoville, K):
     answer = 0
     sc=[]
     for i in scoville:
         heapq.heappush(sc,i)
     
-    def mix(sc):
-        cnt=0
-        while len(sc)>=2:
-            a=heapq.heappop(sc)
-            if a>=K:
-                return 0
-            b=heapq.heappop(sc)
-            heapq.heappush(sc,(a+b*2))
-            cnt+=1
-            temp=heapq.heappop(sc)
-            #print(sc,temp,cnt)
-            if temp>=K:
-                return cnt
-            heapq.heappush(sc,temp)
-        return -1
-    answer=mix(sc)
-    return answer
+    while len(sc)>=2:
+        a=heapq.heappop(sc)
+        if a>=K:
+            return 0
+        b=heapq.heappop(sc)
+        heapq.heappush(sc,a+b*2)
+        answer+=1
+        temp=heapq.heappop(sc)
+        if temp>=K:
+            return answer
+        heapq.heappush(sc,temp)
+        
+    return -1
