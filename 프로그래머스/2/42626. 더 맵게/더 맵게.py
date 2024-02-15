155e7f2173f0f9eb216ce6b498a566dd6d7da3bc
밍 <79096808+0321minji@users.noreply.github.com>
2024-02-15 22:18:48 +0900

    [level 2] Title: 더 맵게, Time: 2335.00 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -9,7 +9,10 @@ def solution(scoville, K):
         a=heapq.heappop(sc)
         if a>=K:
             return 0
+
         b=heapq.heappop(sc)
+        if b==0:
+            return -1
         heapq.heappush(sc,a+b*2)
         answer+=1
         temp=heapq.heappop(sc)
