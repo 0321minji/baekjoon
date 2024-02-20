bea83bb06ad3f72f287a96584fb5ebc73bf41829
밍 <79096808+0321minji@users.noreply.github.com>
2024-02-20 20:57:29 +0900

    [Silver II] Title: DFS와 BFS, Time: 56 ms, Memory: 32236 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,37 +1,38 @@
-from collections import deque
 import sys
-
 input=sys.stdin.readline
 
+def dfs(i):
+    print(i,end=' ')
+    for nxt in graph[i]:
+        if not visit[nxt]:
+            visit[nxt]=1
+            dfs(nxt)
+
+def bfs(i):
+    que=[i]
+    while que:
+        q=que.pop(0)
+        print(q,end=' ')
+        for nxt in graph[q]:
+            if not visit[nxt]:
+                visit[nxt]=1
+                que.append(nxt)
+            
+    
 n,m,v=map(int,input().split())
-link=[[0]*(n+1) for _ in range(n+1)] #간선 행렬로 표현
+graph=[[] for _ in range(n+1)]
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+for i in range(n+1):
+    graph[i].sort()
+
 visit=[0]*(n+1)
-#default값은 0; 연결되거나 지나갔을경우 1
-
-
-for i in range(m):
-    x,y=map(int,input().split())
-    link[x][y]=link[y][x]=1
-
-def dfs(v):
-    visit[v]=1
-    print(v,end=' ')
-    for i in range(1,n+1):
-        if(visit[i]==0 and link[v][i]==1): #방문안했고, 연결되있을 시
-            dfs(i)
-
-def bfs(v):
-    visit[v]=0 #dfs실행 이후 default가 1이 됨->방문하면 0으로
-    q=[v]
-
-    while q:
-        v=q.pop(0)
-        print(v,end=' ')
-        for i in range(1,n+1):
-            if(visit[i]==1 and link[v][i]==1):
-                q.append(i)
-                visit[i]=0
-
+visit[v]=1
 dfs(v)
 print()
+visit=[0]*(n+1)
+visit[v]=1
 bfs(v)
