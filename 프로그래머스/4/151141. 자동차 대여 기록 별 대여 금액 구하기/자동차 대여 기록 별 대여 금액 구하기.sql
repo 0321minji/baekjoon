31555f8e3ca44cc044cfd2490ceb218a59cb7b0e
밍 <79096808+0321minji@users.noreply.github.com>
2024-02-23 21:45:35 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,15 +1,18 @@
--- 트럭의 대여 기록 별로 대여 금액 구하기
-WITH T AS (SELECT C.CAR_ID, C.DAILY_FEE, H.HISTORY_ID,CAR_TYPE, DATEDIFF(END_DATE,START_DATE)+1 AS DURATION, 
-          CASE
-            WHEN DATEDIFF(END_DATE,START_DATE)+1 >=90 THEN '90일 이상'
-            WHEN DATEDIFF(END_DATE,START_DATE)+1 >=30 THEN '30일 이상'
-            WHEN DATEDIFF(END_DATE,START_DATE)+1 >=7 THEN '7일 이상'
-            ELSE 'None' END AS DURATION_TYPE
-          FROM CAR_RENTAL_COMPANY_CAR C JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY H
-          ON C.CAR_ID = H.CAR_ID 
-          WHERE CAR_TYPE='트럭')
-SELECT HISTORY_ID, ROUND(DAILY_FEE*((100-IFNULL(DISCOUNT_RATE,0))/100)*DURATION,0) AS FEE
-# , DISCOUNT_RATE, P.DURATION_TYPE, DURATION
-FROM T LEFT OUTER JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P 
-ON T.DURATION_TYPE = P.DURATION_TYPE AND T.CAR_TYPE=P.CAR_TYPE
-ORDER BY FEE DESC, HISTORY_ID DESC
+-- 코드를 입력하세요
+with truck as (select daily_fee, history_id,datediff(end_date,start_date)+1 as d
+                from car_rental_company_car c 
+                join car_rental_company_rental_history h on c.car_id=h.car_id
+            where car_type='트럭')
+, dr as (            
+select duration_type, discount_rate 
+from car_rental_company_discount_plan p
+where car_type='트럭'
+order by duration_type)
+
+select history_id,round(daily_fee*(d)*(100-ifnull(discount_rate,0))/100) as fee
+from (select *,case when d>=90 then '90일 이상'
+               when d>=30 then '30일 이상'
+               when d>=7 then '7일 이상'
+               else null end as dt from truck t) as tt
+left outer join dr on tt.dt=dr.duration_type
+order by fee desc, history_id desc
