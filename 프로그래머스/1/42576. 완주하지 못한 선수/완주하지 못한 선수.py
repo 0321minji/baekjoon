e21e4a7a1d03dead7d8295d0742aa74574350d5a
밍 <79096808+0321minji@users.noreply.github.com>
2024-07-31 17:15:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.84 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,10 @@
+# import collections
+
+# def solution(participant, completion):
+#     answer = collections.Counter(participant)-collections.Counter(completion)
+#     return list(answer)[0]
 import collections
 
-def solution(participant, completion):
-    answer = collections.Counter(participant)-collections.Counter(completion)
+def solution(participant,completion):
+    answer=collections.Counter(participant)-collections.Counter(completion)
     return list(answer)[0]
