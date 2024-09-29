bfac27afd2b2a349d31aec5236470af496c1907d
밍 <79096808+0321minji@users.noreply.github.com>
2024-09-29 22:28:26 +0900

    [Silver III] Title: 프린터 큐, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,26 +1,27 @@
 import sys
 input=sys.stdin.readline
 
-case=int(input())
-
-for i in range(case):
-    m,n=map(int,input().split())
-    ans=[0]*m; ans[n]=1
-    test=[]
-    test=list(map(int,input().split()))
-    count=0
-    
-    while(1):
-        if(test[0]==max(test)):
-            del test[0]
-            count+=1
-            if(ans[0]==1):
-                print(count)
+t=int(input())
+for _ in range(t):
+    n,m=map(int,input().split())
+    important=list(map(int,input().split()))
+    num=important[m]
+    arr=[]
+    for i in range(n):
+        arr.append([important[i],i])
+    cnt=0
+    while True:
+        # 맨 앞에 있는게 우선순위가 가장 크면
+        if arr[0][0]==max(important):
+            cnt+=1
+            if arr[0][1]==m:
+                print(cnt)
                 break
-            else:
-                del ans[0]
+            important.remove(arr[0][0])
+            arr.remove(arr[0])
+        # 그게 아니면 뒤로 추가
         else:
-            test.append(test[0])
-            del test[0]
-            ans.append(ans[0])
-            del ans[0]
+            arr.append(arr[0])
+            arr.remove(arr[0])
+
+        #print(arr,important)
