855b56ca7ce9dd9df868a6a85bb597009cbe36e6
밍 <79096808+0321minji@users.noreply.github.com>
2024-10-20 00:50:11 +0900

    [Silver I] Title: IPv6, Time: 164 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Silver/3107. IPv6/IPv6.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input.charAt(input.length()-1)==':'){
+            input+='0';
+        }
+        String[] ip = input.split(":");
+        int cnt = 8-ip.length;
+
+        List<String> result = new ArrayList<>();
+        boolean check = false;
+
+        for (String s : ip){
+            int length = s.length();
+            if(length==0){
+                if (!check){
+                    for (int i=0;i<cnt+1;i++){
+                        result.add("0000");
+                    }
+                    check=true;
+                }
+                else{
+                    result.add("0000");
+                }
+            }
+            else{
+                String temp = "0000".substring(length)+s;
+                result.add(temp);
+            }
+        }
+        System.out.println(String.join(":",result));
+        sc.close();
+    }    
+}
