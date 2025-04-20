fc5fd3a8de561ed8225ca0a1d51b5eddf91fa478
밍 <79096808+0321minji@users.noreply.github.com>
2025-04-20 18:46:46 +0900

    [Silver II] Title: DFS와 BFS, Time: 140 ms, Memory: 116320 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,38 @@
 import sys
 input=sys.stdin.readline
+from collections import deque
 
-def dfs(i):
-    print(i,end=' ')
-    for nxt in graph[i]:
+def dfs(now):
+    visit[now]=1
+    print(now,end=' ')
+    for nxt in graph[now]:
         if not visit[nxt]:
-            visit[nxt]=1
             dfs(nxt)
-
-def bfs(i):
-    que=[i]
+          
+def bfs():
+    que=deque([v])
+    visit[v]=1
     while que:
-        q=que.pop(0)
-        print(q,end=' ')
-        for nxt in graph[q]:
+        now = que.popleft()
+        print(now,end=' ')
+        for nxt in graph[now]:
             if not visit[nxt]:
                 visit[nxt]=1
                 que.append(nxt)
-            
-    
-n,m,v=map(int,input().split())
-graph=[[] for _ in range(n+1)]
 
-for _ in range(m):
-    a,b=map(int,input().split())
+n,m,v=map(int,input().split())
+graph=[ [] for _ in range(n+1)]
+edges = [list(map(int,input().split())) for _ in range(m)]
+# edges.sort()
+
+for a,b in edges:
     graph[a].append(b)
     graph[b].append(a)
 for i in range(n+1):
     graph[i].sort()
 
 visit=[0]*(n+1)
-visit[v]=1
 dfs(v)
 print()
 visit=[0]*(n+1)
-visit[v]=1
-bfs(v)
+bfs()
