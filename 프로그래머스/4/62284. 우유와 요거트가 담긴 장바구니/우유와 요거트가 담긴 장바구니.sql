ae4bf7071168a43a94a641fd0c0f7368a32c9b25
밍 <79096808+0321minji@users.noreply.github.com>
2025-06-08 14:46:35 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,31 +1,23 @@
 -- 코드를 입력하세요
 
-# with y as (
-# select cart_id
+# select y.cart_id
+# from (select cart_id
 # from cart_products
 # where name='Yogurt'
-# group by cart_id
-# )
-
-# with m as (
-# select cart_id
+# group by cart_id) as y
+# join 
+# (select cart_id
 # from cart_products
 # where name='Milk'
-# group by cart_id
-# )
+# group by cart_id) as m
+# on
+# y.cart_id = m.cart_id
+# order by y.cart_id
 
-select y.cart_id
-from (select cart_id
+
+select cart_id
 from cart_products
-where name='Yogurt'
-group by cart_id) as y
-join 
-(select cart_id
-from cart_products
-where name='Milk'
-group by cart_id) as m
-on
-y.cart_id = m.cart_id
-
-order by y.cart_id    
-
+where name in ('Yogurt','Milk')
+group by cart_id
+having count(distinct name)=2
+order by cart_id
