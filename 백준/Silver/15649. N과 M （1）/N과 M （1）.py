990d7ab20a5a9d88f36eaefd3a79d8ae906f1f47
밍 <79096808+0321minji@users.noreply.github.com>
2025-08-10 13:00:28 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 119360 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,20 @@
 import sys
 input=sys.stdin.readline
 
-def backtracking(level):
-    global m
-    if level==m:
-        print(*temp[:level])
-    for i in range(1,n+1):
-        if not check[i]:
-            check[i]=1
-            temp[level]=i
-            backtracking(level+1)
-            check[i]=0
-        #print(temp)
-
-
 n,m=map(int,input().split())
-temp=[0]*10
-check=[0]*10
 
-backtracking(0)
+res=[]
+visited=[False]*(n+1)
+def bt(depth,now):
+    if depth==m:
+        res.append(now)
+        return    
+    for i in range(1,n+1):
+        if not visited[i]:
+            visited[i]=True
+            bt(depth+1,now+[i])
+            visited[i]=False
+    
+bt(0,[])
+for r in res:
+    print(*r)
