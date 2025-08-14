f5af26e7802cc742c7d158d15ae09434dce3e8c6
밍 <79096808+0321minji@users.noreply.github.com>
2025-08-15 01:56:24 +0900

    [Platinum V] Title: 등산 마니아, Time: 512 ms, Memory: 194396 KB, Score: 47 point -BaekjoonHub

--- 백준/Platinum/20188. 등산 마니아/등산 마니아.py
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.py
@@ -9,27 +9,23 @@ for _ in range(n-1):
     a,b=map(int,input().split())
     tree[a].append(b)
     tree[b].append(a)
-visited=[False]*(n+1)
-def dfs(index,route,dist):
-    # print(index,dist)
-    if not dp[index] or dp[index][0]>dist:
-        dp[index]=[dist,route]
-        
-    # leaf이면 종료
-    if len(tree[index])==1 and index!=1:
-        return
-    
-    for nxt in tree[index]:
-        if not visited[nxt]:
-            visited[nxt]=True
-            dfs(nxt,route|set([nxt]),dist+1)
-            visited[nxt]=False
 
-dfs(1,set(),0)
-# print(dp)
 res=0
-for i in range(1,n+1):
-    for j in range(i+1,n+1):
-        # print(i,j,dp[i][1]|dp[j][1])
-        res+=len(dp[i][1]|dp[j][1])
+def dfs(index,parent):
+    global res
+    size=1
+    
+    for v in tree[index]:
+        if v==parent:
+            continue
+        # 자기 밑의 정점 수 세기
+        cnt=dfs(v,index)
+        # 양 쪽에서
+        res+=cnt*(n-cnt)
+        # 밑 에서
+        res+=cnt*(cnt-1)//2
+        size+=cnt
+    return size
+    
+dfs(1,0)
 print(res)
